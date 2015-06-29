80bfaf7304152b4326a20abe74fb5057db647508
a25patel <aanaletap2510@gmail.com>
2015-06-29 14:37:36 -0600

    All Login validations complete, excluding check username existance

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var bcrypt = require('bcryptjs');
 //var key = process.env.PIVOTAL_API;
 
 
-/* GET home page. */
+/* GET Index page. */
 router.get('/', function(req, res, next) {
   paletteCollection.find({}, function(err, palettes){
     res.render('index', { title: 'Color Combos' });
@@ -17,6 +17,10 @@ router.get('/', function(req, res, next) {
 router.get('/home', function(req,res,next){
   res.render('home');
 });
+
+router.get('/directory', function(req,res,next){
+  res.render('directory'); 
+})
 
 // Check Validations for Create Account
 router.post('/create', function(req, res, next){
@@ -31,8 +35,8 @@ router.post('/create', function(req, res, next){
     // Password length must be 8 characters
   } else if (req.body.newPassword.length < 8 || req.body.newPassword.length > 30){
      array.push('Password must be 8 characters long, but less than 30 characters');
-    // password must not match email
-  } else if (req.body.newPassword === req.body.newEmail){
+    // password must not match username
+  } else if (req.body.newPassword === req.body.newUsername){
       array.push('Password cannot be email')
   }
 
@@ -48,8 +52,9 @@ router.post('/create', function(req, res, next){
   if(array.length > 0){
     console.log(array);
     res.render('', { title: 'Color Combos', errors: array})
-  }
+  } //ELSE
   else {
+    // IF username already exists render errror
     paletteCollection.find({}, function(err, palettes){
       var errorArray = [];
       for(var i =0; i< palettes.length; i++){
@@ -58,6 +63,7 @@ router.post('/create', function(req, res, next){
           res.render('', {title: 'Color Combos', errors: errorArray})
         }
       }
+      // if errorArray OR palettes length is 0, insert new user info into database
       if (errorArray.length === 0 || palettes.length === 0) {
         paletteCollection.insert({
           username: req.body.newUsername,
@@ -70,4 +76,44 @@ router.post('/create', function(req, res, next){
   }
 });
 
+
+// Validations for LOGIN Page
+router.post('/login', function(req, res, next){
+  var array = [];
+  if (req.body.username === ''){
+    array.push('Username cannot be blank')
+  }
+  if (req.body.password === ''){
+    array.push('Password cannot be blank')
+  }
+  if (array.length > 0){
+    res.render('' , {title: 'Color Combos' , errors: array})
+  } else {
+
+    // paletteCollection.find({}, function(err, palettes){
+    //   var array = [];
+    //   var userArray =[];
+    //   for(var i = 0; i< palettes.length; i++){
+    //     if (req.body.user != palettes[i].username){
+    //       userArray.push('Username does not exist')
+    //     }
+    //   }
+    //   array.push(userArray[0])
+    //   res.render('' , {title: 'Color Combos' , errors: array})
+    // })
+
+    paletteCollection.findOne({username:req.body.username}, function(err,palettes){
+      var array = [];
+      if(bcrypt.compareSync(req.body.password , palettes.password) === false){
+        array.push('Invalid Password')
+        res.render('' , {title: 'Color Combos' , errors:array})
+      } else {
+        res.redirect('/home');
+      }
+    })
+  }
+
+
+
+});
 module.exports = router;
